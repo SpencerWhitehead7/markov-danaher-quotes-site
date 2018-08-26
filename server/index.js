3fcba8379e9c97e308de712efbb45ea237e4a7f5
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-26 15:56:00 -0400

    feat: 404 handling

--- server/index.js
+++ server/index.js
@@ -18,4 +18,8 @@ app.use((err, req, res, next) => {
   res.status(500).send(`Mistakes were made.`)
 })
 
+app.use((req, res, next) => {
+  res.status(404).send(`Nothing here. Sorry. Also, how did you get past the res.redirect?`)
+})
+
 app.listen(1337, () => console.log(`partying hard on http://localhost:1337/`))
