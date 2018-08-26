0cdb4f44a845a84d133fcdedb500237df63e2857
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-26 15:55:02 -0400

    feat: main server route

--- server/index.js
+++ server/index.js
@@ -7,4 +7,6 @@ app.use(volleyball)
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
+app.use(`/`, express.static(path.join(__dirname, `../client`)))
+
 app.listen(1337, () => console.log(`partying hard on http://localhost:1337/`))
