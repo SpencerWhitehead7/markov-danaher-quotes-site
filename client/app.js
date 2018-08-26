29953187c50619bc95d1c932d8c9e52ed5972b40
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-26 19:07:51 -0400

    feat: requests quotes from server

--- client/app.js
+++ client/app.js
@@ -1,16 +1,22 @@
 const generateQuoteButton = document.getElementsByTagName(`button`)[0]
 const selector = document.getElementsByTagName(`select`)[0]
+const wisdom = document.getElementById(`wisdom`)
 
 let length = 1
 
-generateQuoteButton.addEventListener(`click`, event => {
-  console.log(`button attached!`)
-  event.preventDefault()
-})
-
 selector.addEventListener(`change`, event => {
-  console.log(`selector attached`)
   event.preventDefault()
   length = event.target.value
+})
+
+generateQuoteButton.addEventListener(`click`, async event => {
+  event.preventDefault()
+  try{
+    const quoteData = await fetch(`/api/${length}`, {method : `POST`})
+    const quote = await quoteData.text()
+    wisdom.innerHTML = quote
+  }catch(error){
+    console.log(error)
+  }
 })
   
