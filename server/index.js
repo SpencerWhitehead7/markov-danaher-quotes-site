e844afaa22f9902b4b2000ced5f2565621a10c36
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-26 22:12:25 -0400

    refactor: error handling

--- server/index.js
+++ server/index.js
@@ -19,8 +19,9 @@ app.get(`/*`, (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
+  console.error(err)
   console.error(err.stack)
-  res.status(500).send(`Mistakes were made.`)
+  res.status(err.status || 500).send(err.message || `Internal Server Error`)
 })
 
 app.use((req, res, next) => {
